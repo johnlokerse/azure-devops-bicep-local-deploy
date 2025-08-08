7895e17e326fef961a74e37ce57d8aa827a9819f
John <J.G.Lokerse@GMail.com>
2025-08-08 19:52:45 +0200

    test with paths

--- Infra/Scripts/Publish-Extension.ps1
+++ Infra/Scripts/Publish-Extension.ps1
@@ -23,8 +23,10 @@ ExecSafe { dotnet publish --configuration Release $root -r win-x64 }
 
 # publish to the registry
 ExecSafe { ~/.azure/bin/bicep publish-extension `
-  --bin-osx-arm64 "$root/src/bin/Release/osx-arm64/publish/$extName" `
-  --bin-linux-x64 "$root/src/bin/Release/linux-x64/publish/$extName" `
-  --bin-win-x64 "$root/src/bin/Release/win-x64/publish/$extName.exe" `
+  --bin-osx-arm64 "src/bin/Release/osx-arm64/publish/$extName" `
+  --bin-linux-x64 "src/bin/Release/linux-x64/publish/$extName" `
+  --bin-win-x64 "src/bin/Release/win-x64/publish/$extName.exe" `
   --target "$Target" `
   --force }
+
+Write-Host "Path: $root"

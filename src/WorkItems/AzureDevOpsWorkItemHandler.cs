a5425a512dcc50845cd3100835cbc61e737cb34e
Shpend Kelmendi <shekel7@hotmail.com>
2025-10-31 10:39:38 +0100

    Remove preview method (not value yet)

--- src/WorkItems/AzureDevOpsWorkItemHandler.cs
+++ src/WorkItems/AzureDevOpsWorkItemHandler.cs
@@ -3,21 +3,6 @@ namespace DevOpsExtension.WorkItems;
 public class AzureDevOpsWorkItemHandler
     : AzureDevOpsResourceHandlerBase<AzureDevOpsWorkItem, AzureDevOpsWorkItemIdentifiers>
 {
-    protected override async Task<ResourceResponse> Preview(
-        ResourceRequest request,
-        CancellationToken cancellationToken)
-    {
-        var props = request.Properties;
-
-        var client = CreateClient(request, props);
-
-        var existing = await client.FindByAsync(props.Id, cancellationToken);
-
-        if (existing is not null) props.Id = existing.Value;
-
-        return GetResponse(request);
-    }
-
     protected override async Task<ResourceResponse> CreateOrUpdate(
         ResourceRequest request,
         CancellationToken cancellationToken)
@@ -29,9 +14,9 @@ public class AzureDevOpsWorkItemHandler
         var existing = await client.FindByAsync(props.Id, cancellationToken);
 
         if (existing is null)
-            await client.CreateAsync(props.Id, props.Title, props.Type, cancellationToken);
+            await client.CreateAsync(props.Id, props.Title, props.Type, props.Description, cancellationToken);
         else
-            await client.UpdateAsync(existing.Value, props.Title, cancellationToken);
+            await client.UpdateAsync(existing.Value, props.Title, props.Type, props.Description, cancellationToken);
 
         return GetResponse(request);
     }

494e5c25fbfccc95d90d553938765d2ce44ae403
John <J.G.Lokerse@GMail.com>
2025-12-21 16:55:02 +0100

    Updated powershell script to dynamically retreive dotnet version

--- Infra/Scripts/Publish-Extension.ps1
+++ Infra/Scripts/Publish-Extension.ps1
@@ -1,7 +1,7 @@
 #!/usr/bin/env pwsh
 [cmdletbinding()]
 param(
-   [Parameter(Mandatory=$true)][string]$Target
+  [Parameter(Mandatory = $true)][string]$Target
 )
 
 $ErrorActionPreference = "Stop"
@@ -9,12 +9,19 @@ $ErrorActionPreference = "Stop"
 function ExecSafe([scriptblock] $ScriptBlock) {
   & $ScriptBlock
   if ($LASTEXITCODE -ne 0) {
-      exit $LASTEXITCODE
+    exit $LASTEXITCODE
   }
 }
 
 $root = "$PSScriptRoot/../.."
 $extName = "azure-devops-extension"
+
+# Get the target framework from the project file
+$csprojPath = "$root/src/DevOpsExtension.csproj"
+[xml]$csproj = Get-Content $csprojPath
+$targetFramework = $csproj.Project.PropertyGroup.TargetFramework | Where-Object { $_ } | Select-Object -First 1
+
+Write-Host "Building for target framework: $targetFramework"
 
 # build various flavors
 ExecSafe { dotnet publish --configuration Release $root -r osx-arm64 }
@@ -24,9 +31,9 @@ ExecSafe { dotnet publish --configuration Release $root -r win-x64 }
 
 # publish to the registry
 ExecSafe { ~/.azure/bin/bicep publish-extension `
-  --bin-osx-arm64 "$root/src/bin/Release/net9.0/osx-arm64/publish/$extName" `
-  --bin-osx-x64 "$root/src/bin/Release/net9.0/osx-x64/publish/$extName" `
-  --bin-linux-x64 "$root/src/bin/Release/net9.0/linux-x64/publish/$extName" `
-  --bin-win-x64 "$root/src/bin/Release/net9.0/win-x64/publish/$extName.exe" `
-  --target "$Target" `
-  --force }
+    --bin-osx-arm64 "$root/src/bin/Release/$targetFramework/osx-arm64/publish/$extName" `
+    --bin-osx-x64 "$root/src/bin/Release/$targetFramework/osx-x64/publish/$extName" `
+    --bin-linux-x64 "$root/src/bin/Release/$targetFramework/linux-x64/publish/$extName" `
+    --bin-win-x64 "$root/src/bin/Release/$targetFramework/win-x64/publish/$extName.exe" `
+    --target "$Target" `
+    --force }
